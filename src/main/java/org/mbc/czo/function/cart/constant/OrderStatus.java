ed8b4ec6a32e02cae0e421ebeb1b1aa172ca4180
220-08\gusckd1009 <gusckd1009@gmail.com>
2025-08-13 10:14:33 +0900

    zerowaste 그룹 프로젝트 1차 백업

---
+++ src/main/java/org/mbc/czo/function/cart/constant/OrderStatus.java
@@ -0,0 +1,5 @@
+package org.mbc.czo.function.cart.constant;
+
+public class OrderStatus {
+    ORDER, CANCEL
+}
---
+++ src/main/java/org/mbc/czo/function/cart/repository/CartRepository.java
@@ -0,0 +1,11 @@
+package org.mbc.czo.function.cart.repository;
+
+import org.mbc.czo.function.cart.domain.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Cart findByMemberId(Long memberId);
+
+}
